5a3ab01edf79c2239f391e9e9e958da0acae9297
Shane Moyo <smoyo@pdx.edu>
2017-10-20 09:33:40 -0700

    Finished studio schema tests

--- test/unit/studio.test.js
+++ test/unit/studio.test.js
@@ -23,18 +23,4 @@ describe('Studio model', () => {
         assert.equal(errors.name.kind, 'required');
     });
 
-    it('Validates bad model', () => {
-        const studio = new Studio({
-            name: 'Fox',
-            address: {
-                city: 'Los Angeles',
-                state: 'California',
-                country: 'USA'
-            }
-        });
-        const { errors } = studio.validateSync();
-        console.log('I AM THE ERRORS', errors);
-        assert.equal(errors.name.kind, 'string');
-    });
-
 });
