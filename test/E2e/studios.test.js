5749f3263db82a18830da50a710933ecebbcdfd5
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-20 12:05:26 -0700

    passing get all test and method

--- test/E2e/studios.test.js
+++ test/E2e/studios.test.js
@@ -8,6 +8,9 @@ describe('studios API', () => {
     const warner = {
         name: 'Warner'
     };
+    const fox = {
+        name: 'Fox'
+    };
 
     it('saves a studio with ID', () => {
         return request.post('/api/studios')
@@ -16,6 +19,30 @@ describe('studios API', () => {
                 const studio = res.body;
                 assert.ok(studio._id);
                 assert.equal(studio.name, warner.name);
+            });
+    });
+
+    it('Gets all studios', () =>{
+        const saves = [ warner, fox].map(studio =>{
+            return request.post('/api/studios')
+                .send(studio)
+                .then(res => res.body);
+        });
+        let saved = null;
+        let savedNames = null;
+        return Promise.all(saves)
+            .then(_saved => {
+                saved = _saved;
+                savedNames = saved.map( save => { 
+                    return {
+                        _id: save._id,
+                        name: save.name
+                    }; 
+                });
+                return request.get('/api/studios');
+            })
+            .then(res =>{
+                assert.deepEqual(res.body, savedNames);
             });
     });
 });
