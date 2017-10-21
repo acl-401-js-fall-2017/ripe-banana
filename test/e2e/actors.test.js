e94a53caef561c06bd236a4386409e5ec1d17485
mirrorapps <eli.speigel@gmail.com>
2017-10-21 12:05:22 -0700

    post actor by id test fails

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -5,5 +5,32 @@ const request = require('./request');
 describe('actor CRUD', () => {
     let rawData = null;
 
-    beforeEach(() => mongoose.dropDatabase());
+    beforeEach(() => {
+        mongoose.dropDatabase();
+        rawData = [
+            {
+                name: 'Steven',
+                dob: new Date('2017-10-20'),
+                pob: '30th NW 10th Ave, Portland, Oregon 97209'
+
+            },
+            {
+                name: 'Oprah'
+            }
+        ];
+    });
+
+    describe('POST Actor', () => {
+        it('returns actor with new id', () => {
+            return request.post('/api/actors')
+                .send(rawData[0])
+                .then(res => assert.ok(res.body._id));
+        });
+    });
+
+    // describe('GET Actor', () => {
+    //     it('returns all when no id', () => {
+    //         return request.
+    //     })
+    // })
 });
