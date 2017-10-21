2e48147704102394fa84b80279a25f6ab4415511
mirrorapps <eli.speigel@gmail.com>
2017-10-21 13:59:43 -0700

    reviewer valid model test passing

---
+++ test/unit/Reviewer.test.js
@@ -0,0 +1,17 @@
+const {assert} = require('chai');
+const Reviewer = require('../../lib/models/Reviewer');
+
+describe('Reviewer model', () => {
+    let rawData = null;
+
+    beforeEach(() => rawData = {
+        name: 'Mel',
+        company: 'film blog of mel'
+    });
+
+    it('valid model', () => {
+        const mel = new Reviewer(rawData);
+        assert.ok(!mel.validateSync());
+        assert.ok(mel.name);
+    });
+});
