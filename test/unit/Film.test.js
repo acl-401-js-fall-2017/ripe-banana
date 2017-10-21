09c6b280dc4003a584d22798def9d92ce4f4acec
mirrorapps <eli.speigel@gmail.com>
2017-10-21 13:43:59 -0700

    created film schema and setup film tests

---
+++ test/unit/Film.test.js
@@ -0,0 +1,24 @@
+const {assert} = require('chai')
+const Studio = require('../../lib/models/Studio');
+const Actor = require('../../lib/models/Actor');
+
+describe('Film model', () => {
+    
+    before(() => {
+        const instStudio = new Studio({name: 'paramount'});
+        const instActor = new Actor({name: 'jack'});
+        const instActor2 = new Actor({name: 'jill'});
+    });
+    
+    let rawData = null;
+
+    beforeEach(() => rawData = {
+        title: 'Halloween',
+        studio: instStudio._id,
+        cast: [
+            instActor._id,
+            instActor2._id
+        ]
+    });
+
+});
