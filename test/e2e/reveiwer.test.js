8120eebb78bf3b5283406c1f5d8955a3398cf978
Shane Moyo <smoyo@pdx.edu>
2017-10-22 11:05:13 -0700

    added failing save reviewer test

---
+++ test/e2e/reveiwer.test.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const request = require('./request');
+
+
+describe('reviewer API', () => {
+    beforeEach(()=> mongoose.connection.dropDatabase());
+
+    const kateTaylor = {
+        name: 'Kate Taylor',
+        company: 'Globe and Mail'
+    };
+
+    it('saves a reviewer with id', ()=>{
+        return request.post('/api/reviewers')
+            .send(kateTaylor)
+            .then(res => {
+                const reviewer = res.body;
+                assert.ok(reviewer._id);
+                assert.equal(reviewer.name, kateTaylor.name);
+            })
+    })
+
+
+})
+
