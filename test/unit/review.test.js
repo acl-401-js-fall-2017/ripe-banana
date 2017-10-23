2a125c58ad80580122b45dcec4014a7e2c452c04
Shane Moyo <smoyo@pdx.edu>
2017-10-22 17:03:29 -0700

    validates a good review model passing

---
+++ test/unit/review.test.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const Review = require('../../lib/models/review');
+const Reviewer = require('../../lib/models/reviewer');
+
+describe('review model', () => {
+
+    const testReviewer = new Reviewer({
+        name: 'Kate Taylor',
+        company: 'Globe and Mail'
+    });
+    console.log('I AM THE TEST REVIEWER =============', testReviewer)
+
+    it('Validates a good model ', () => {
+        const review = new Review({
+            rating: 5,
+            reviewer: testReviewer._id,
+            reviewText: 'this movie sucks'
+        });
+        assert.equal(review.validateSync(), undefined);
+    });
+
+});
