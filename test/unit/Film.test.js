138466116aff8d697db8d67ed58e8b0d8c7f4a0c
David Nishikawa <triptype@gmail.com>
2017-10-22 17:59:53 -0700

    pass green test for create valid film with id

--- test/unit/Film.test.js
+++ test/unit/Film.test.js
@@ -1,24 +1,43 @@
 const {assert} = require('chai')
 const Studio = require('../../lib/models/Studio');
 const Actor = require('../../lib/models/Actor');
+const Film = require('../../lib/models/Film');
 
 describe('Film model', () => {
     
+    let instStudio = null;
+    let instActor = null;
+    let instActor2 = null;
     before(() => {
-        const instStudio = new Studio({name: 'paramount'});
-        const instActor = new Actor({name: 'jack'});
-        const instActor2 = new Actor({name: 'jill'});
+        instStudio = new Studio({name: 'paramount'});
+        instActor = new Actor({name: 'jack'});
+        instActor2 = new Actor({name: 'jill'});
     });
     
     let rawData = null;
 
-    beforeEach(() => rawData = {
-        title: 'Halloween',
-        studio: instStudio._id,
-        cast: [
-            instActor._id,
-            instActor2._id
-        ]
+    beforeEach(() => {
+        rawData = {
+            title: 'Halloween',
+            studio: instStudio._id,
+            released: 2000,
+            cast: [
+                {
+                    part: 'damsel in distress',
+                    actor: instActor._id
+                },
+                {
+                    part: 'lead',
+                    actor: instActor2._id
+                }
+            ]
+        };
     });
+
+    it('constructs a valid film with a new mongo id', () => {
+        const newMov = new Film(rawData);
+        assert.ok(newMov._id);
+        assert.ok(!newMov.validateSync());
+    })
 
 });
