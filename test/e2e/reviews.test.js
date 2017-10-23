434ce3a0ca1ca46939a853f628a29534ab119944
Shane Moyo <smoyo@pdx.edu>
2017-10-22 17:45:33 -0700

    added failing get all reviews test

--- test/e2e/reviews.test.js
+++ test/e2e/reviews.test.js
@@ -4,7 +4,9 @@ const request = require('./request');
 
 describe('review API', () => {
 
-    let testReview = null;
+    let testReview1 = null;
+    let testReview2 = null;
+    let testReview3 = null;
 
     beforeEach(() => {
         mongoose.connection.dropDatabase();
@@ -18,21 +20,55 @@ describe('review API', () => {
             .send(reviewer)
             .then(res => {
                 const testReviewer = res.body;
-                testReview = {
-                    rating: 5,
+                testReview1 = {
+                    rating: 1,
                     reviewer: testReviewer._id,
                     reviewText: 'this movie sucks'
+                };
+                testReview2 = {
+                    rating: 2,
+                    reviewer: testReviewer._id,
+                    reviewText: 'this movie is poo'
+                };
+                testReview3 = {
+                    rating: 5,
+                    reviewer: testReviewer._id,
+                    reviewText: 'this movie is great'
                 };
             });
     });
 
     it('saves a review with id', ()=>{
         return request.post('/api/reviews')
-            .send(testReview)
+            .send(testReview1)
             .then(res => {
                 const review = res.body;
                 assert.ok(review._id);
-                assert.equal(review.reviewer, testReview.reviewer);
+                assert.equal(review.reviewer, testReview1.reviewer);
+            });
+    });
+
+    it('Gets all reviews', () =>{
+        const saves = [testReview1, testReview2, testReview3].map(review =>{
+            return request.post('/api/reviews')
+                .send(review)
+                .then(res => res.body);
+        });
+        let saved = null;
+        let savedData = null;
+        return Promise.all(saves)
+            .then(_saved => {
+                saved = _saved;
+                savedData = saved.map( save => {
+                    return {
+                        rating: save.rating,
+                        reviewText: save.reviewText
+                    };
+                });
+                return request.get('/api/reviews');
+            })
+            .then(res =>{
+                assert.deepEqual(res.body, savedData);
             });
     });
 
