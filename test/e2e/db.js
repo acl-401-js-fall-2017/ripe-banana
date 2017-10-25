230b6cdedfcd4c1b2d0a4189573bf5ea46526bad
Zac-L <Zachlichens@gmail.com>
2017-10-25 12:59:27 -0700

    setup a handler function

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const connect = require('../../lib/connect');
 const url = 'mongodb://localhost:27017/ripe-banana-test';
+const request = require('./request'); // eslint-disable-line
 
 before(() => connect(url));
 after(() => mongoose.connection.close());
+
+
+module.exports = {
+    drop() {
+        return mongoose.connection.dropDatabase();
+    },
+    getToken(user = {
+        email: 'me@me.com',
+        password: 'four'
+    })
+    {
+        return request.post('/api/auth/signup')
+            .send(user)
+            .then(({ body }) => body.token);
+    }
+};
