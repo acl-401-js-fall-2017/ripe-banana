bb6589bb2f856e833207f64dc8ff7c0660622a81
Zac-L <Zachlichens@gmail.com>
2017-10-25 12:59:11 -0700

    built out a currently failing test

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,25 @@
+const request = require('./request');
+const { assert } = require('chai');
+const db = require('./db');
+
+describe('Auth API', () => {
+
+    beforeEach(db.drop);
+
+    let token = null;
+
+    beforeEach(() => {
+        return request.post('/api/auth/signup')
+            .send({
+                email: 'user@email.com',
+                password: 'four',
+                name: 'Zach',
+                company: 'Alchemy Code Labs'
+            })
+            .then(({ body }) => token = body.token);
+    });
+
+    it.only('signup', () => {
+        assert.ok(token);
+    });
+});
