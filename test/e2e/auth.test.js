2dadca291e60710ff360f734a5755c459cb2029f
Zac-L <Zachlichens@gmail.com>
2017-10-26 09:45:17 -0700

    added a test to check if user is trying to signup with same email addres that exists already

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -2,7 +2,7 @@ const request = require('./request');
 const { assert } = require('chai');
 const db = require('./db');
 
-describe('Auth API', () => {
+describe.only('Auth API', () => {
 
     beforeEach(db.drop);
 
@@ -14,7 +14,7 @@ describe('Auth API', () => {
                 email: 'user@email.com',
                 password: 'four',
                 name: 'Zach',
-                company: 'Alchemy Code Labs'
+                company: 'Alchemy Code Lab'
             })
             .then(({body}) =>  {
                 token = body.token;
@@ -24,4 +24,21 @@ describe('Auth API', () => {
     it('signup', () => {
         assert.ok(token);
     });
+
+    it('Can not signup with same email', () => {
+        return request.post('/api/auth/signup')
+            .send({
+                email: 'user@email.com',
+                password: 'fourfour',
+                name: 'Zach',
+                company: 'Alchemy Code Lab'
+            })
+            .then(
+                () => { throw new Error('Unexpected successful response'); },
+                err => {
+                    assert.equal(err.status, 400);
+                }
+            );
+    });
+
 });
