86fc2006fba4eb10e20414b0e9f4d58eccd37142
Shane Moyo <smoyo@pdx.edu>
2017-10-26 10:02:10 -0700

    added more schema validation tests from reviewer

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -3,6 +3,23 @@ const User = require('../../lib/models/user');
 
 describe('user model', () => {
 
+    it.only('Validates a good user model', () => {
+        const user = new User({
+            name: 'Shane Moyo',
+            company: 'Moyo reviews',
+            email: 'Shane@me.com',
+            hash: 'Proxy Hash',
+            roles: ['user']
+        });
+        assert.equal(user.validateSync(), undefined);
+    });
+
+    it('checks for required fields', ()=> {
+        const user = new User({});
+        const { errors } = user.validateSync(); 
+        assert.equal(errors.name.kind, 'required');
+    });
+
     const user = new User({
         email: 'shane@me.com'
     });
@@ -15,7 +32,7 @@ describe('user model', () => {
         assert.notEqual(user.hash, password);
     });
 
-    it.only('compares password', () => {
+    it('compares password', () => {
         user.generateHash(password);
         assert.isTrue(user.comparePassword('abc'));
         assert.isFalse(user.comparePassword('bad password'));
